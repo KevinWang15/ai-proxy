db6c7aa76de2b24f4623b6f646f710becf8f9069
Kevin Wang <kevin.ke.wang@airwallex.com>
2025-02-02 21:53:56 +0800

    updates

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const {PageHandler} = require('./page-handler');
 const {BrowserManager} = require('./browser-manager');
 
 async function main() {
+    const nodeVersion = process.versions.node;
+    const majorVersion = parseInt(nodeVersion.split('.')[0], 10);
+    if (majorVersion < 18) {
+        console.error('\x1b[31mError: This application requires Node.js version 18 or higher.\x1b[0m');
+        console.error(`Current version: ${nodeVersion}`);
+        console.error('Please upgrade your Node.js installation.');
+        process.exit(1);
+    }
+
     try {
         const config = await getConfig();
         const browserSetup = new BrowserSetup();
