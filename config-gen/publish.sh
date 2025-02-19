bef4bbad6998edadcf3455d3203e5beee1965034
Ke Wang <me@ke.wang>
2025-02-19 13:56:37 +0800

    update publish.sh

--- config-gen/publish.sh
+++ config-gen/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 node gen.js > config.json
-scp ./config.json root@47.92.23.19:/root/secretdocs/public/ai-config.json
+scp ./config.json root@47.92.23.19:/root/secretdocs/public/ai-config-1.json
